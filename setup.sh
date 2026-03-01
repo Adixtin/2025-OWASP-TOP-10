7c389e47cf68e9422371f39d283452b60ca239d9
Adixtin <adam@at3.pl>
2026-03-01 19:01:27 +0000

    README

--- setup.sh
+++ setup.sh
@@ -4,11 +4,19 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 
-python3 BrokenAccessControl/app.py &
-python3 CryptographicFailures/crypto_app/app.py & 
-python3 Injection/sql_injection/app.py & 
-python3 InsecureDesign/client_side_auth/app.py &
-python3 AuthFailures/no_acc_lockout/app.py & 
-python3 SoftwareOrDataIntegrityFailures/tampered_config/app.py &
-python3 SecurityLoggingAndAlertingFailures/logging/app.py &
-python3 MisshandlingOfExceptionsConditions/exception/app.py & 
+echo "BAC"
+python3 BrokenAccessControl/app.py 
+echo "CF"
+python3 CryptographicFailures/crypto_app/app.py 
+echo "I"
+python3 Injection/sql_injection/app.py 
+echo "IS"
+python3 InsecureDesign/client_side_auth/app.py 
+echo "AF"
+python3 AuthFailures/no_acc_lockout/app.py 
+echo "SODIF"
+python3 SoftwareOrDataIntegrityFailures/tampered_config/app.py 
+echo "SLAAF"
+python3 SecurityLoggingAndAlertingFailures/logging/app.py 
+echo "MOEC"
+python3 MisshandlingOfExceptionsConditions/exception/app.py 
