8cd5d58ce26c70e9549c7a8389bfed4f1b0f94b7
Adixtin <adam@at3.pl>
2026-03-01 12:27:01 +0000

    cryptographic failures markdown

--- CryptographicFailures/crypto_app/test.py
+++
@@ -1,9 +0,0 @@
-from crypto_utils import encrypt_data, decrypt_data
-
-secret = "My SSN is 123-45-6789"
-
-encrypted = encrypt_data(secret)
-print("Encrypted:", encrypted)
-
-decrypted = decrypt_data(encrypted)
-print("Decrypted:", decrypted)
